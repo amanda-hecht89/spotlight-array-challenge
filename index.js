c154b24a2d4a26c2673d0e388ee85d25bf084651
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-24 15:52:07 -0600

    9

--- index.js
+++ index.js
@@ -102,6 +102,8 @@ export const totalSneakerCount = ( brands ) => {
 // convertToArray({}) => []
 // Source: https://edabit.com/challenge/pPNAs5PvB3WvnDwDM
 
-export const convertToArray = () => {}
+export const convertToArray = (object) => {
+  return Object.entries(object)
+}
 
 //
