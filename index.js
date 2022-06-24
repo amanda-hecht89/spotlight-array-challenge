2d1efb70e1eaf4beee94be69f9655fd907a8b99e
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-24 15:28:33 -0600

    7

--- index.js
+++ index.js
@@ -83,7 +83,9 @@ const shoeLegnth = shoes.length
 // INPUT: brands from data.js
 // OUTPUT: the brand names listed
 // REQS: use Object.keys to solve
-export const getBrandNames = () => {}
+export const getBrandNames = ( brands ) => {
+  return(Object.keys(brands));
+}
 
 // INPUT: brands from data.js
 // OUTPUT: total number of sneaker types across all brands (14)
